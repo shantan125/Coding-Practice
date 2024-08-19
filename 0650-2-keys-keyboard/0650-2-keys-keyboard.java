c9a8cabad2201f4644422e6edde77bf2a7aa4aa8
Yeddula Shantan <100433099+shantan125@users.noreply.github.com>
2024-08-19 11:40:27 +0530

    Time: 0 ms (100%), Space: 40.5 MB (55.42%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,15 +1,19 @@
 class Solution {
     public int minSteps(int n) {
-        int operations = 0;
-
-        for (int factor = 2; factor <= n; factor++) {
-            while (n % factor == 0) {
-                operations += factor;
-
-                n /= factor;
+        int steps = 0;
+        
+        // Iterate over possible factors
+        for (int i = 2; i <= n; i++) {
+            // While i is a divisor of n
+            while (n % i == 0) {
+                // Add the factor i to the steps
+                steps += i;
+                
+                // Divide n by i
+                n /= i;
             }
         }
-
-        return operations;
+        
+        return steps;
     }
 }
