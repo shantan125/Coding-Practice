469a52cbd79a5bc5b0fa976a825f9fe5f04bee5c
Yeddula Shantan <yshantaninfo@gmail.com>
2024-09-19 17:48:28 +0530

    Time: 5 ms (6.99%), Space: 44.9 MB (13.74%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,19 +2,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         Set<Integer> s = new TreeSet<>();
-        
-        // Add all elements to the TreeSet (automatically removes duplicates and sorts)
-        for (int num : nums) {
+                for (int num : nums) {
             s.add(num);
         }
-        
-        // Transfer unique elements back to the original array `nums`
         int c = 0;
         for (Integer e : s) {
-            nums[c] = e; // Use assignment operator = instead of ==
+            nums[c] = e; 
             c++;
         }
-        
-        return c; // Return the length of the array with unique elements
+        return c;
     }
 }
